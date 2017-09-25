99e086ef2dadc84a8d7e2693d1d184ac4cf9f382
BoXu <bxu@liquidcap.com>
2017-09-25 17:34:33 +0800

    add dev vnpy path to sys.path, so we can use local vnpy code to debug

--- examples/VnTrader/run.py
+++ examples/VnTrader/run.py
@@ -4,6 +4,9 @@
 import sys
 reload(sys)
 sys.setdefaultencoding('utf8')
+import os
+# add dev vnpy path to search modules
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..\..')))
 
 # 判断操作系统
 import platform
